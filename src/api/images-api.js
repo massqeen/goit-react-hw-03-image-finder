9a529516bb41fdec72f887c9625bd11ce1f644b1
massqeen <62159695+massqeen@users.noreply.github.com>
2020-12-12 20:23:50 +0200

    added URL const

--- src/api/images-api.js
+++ src/api/images-api.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 const API_KEY = '18257903-4453e2975e3dd917fd04b41f9';
+const URL = 'https://pixabay.com/api';
 
 const fetchAPI = ({ searchQuery = '', page = 1, pageSize = 12 }) => {
   return fetch(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&per_page=${pageSize}&page=${page}
+    `${URL}/?key=${API_KEY}&q=${searchQuery}&per_page=${pageSize}&page=${page}
       &image_type=photo&orientation=horizontal`
   )
     .then((res) => res.json())
